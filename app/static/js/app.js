c69a1f81e9dc94cde9099c19180e5e4abbbce065
saung <sithu_aung@intuit.com>
2016-03-15 14:38:12 -0700

    add ngCookies and fix navbar links

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,30 +1,32 @@
 // Declare app level module which depends on filters, and services
-var prodmgmt = angular.module('prodmgmt', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date'])
+var prodmgmt = angular.module('prodmgmt', ['ngResource','ngCookies','ngRoute', 'ui.bootstrap', 'ui.date'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/home/home.html',
-        access: {restricted: true}
+        needLogin: true
       })
       .when('/login', {
       	templateUrl: 'views/auth/login.html',
       	controller: 'loginController',
-      	access: {restricted: false}
+      	needLogin: false
       })
       .when('/logout', {
       	controller: 'logoutController',
-      	access: {restricted: true}
+      	needLogin: true
       })
-      .otherwise({redirectTo: '/', restricted: false});
+      .otherwise({redirectTo: '/', needLogin: true});
   }]);
 
 prodmgmt.run(function ($rootScope, $location, $route, AuthService) {
-  $rootScope.$on('$routeChangeStart', function (event, next, current) {
-    if (next.access === undefined) {
-        console.log("Restricted attribute not present");
-    } else if (next.access.restricted && AuthService.isLoggedIn() === false) {
+  $rootScope.$on('$routeChangeStart', function (event, toState, toParams, fromState, fromParams) {
+
+    if (toState.needLogin && AuthService.isLoggedIn() === false) {
+      console.log("Ypou must connect before you access to this url!!");
+      event.preventDefault();
       $location.path('/login');
-      $route.reload();
-    }
+      // $route.reload();
+    } 
+
   });
 });
