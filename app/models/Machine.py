59142e54283b3874143c35fee3a4d267826ae409
Sithu Aung <sithu_aung@intuit.com>
2017-02-21 00:44:26 -0800

    add machine work

--- app/models/Machine.py
+++ app/models/Machine.py
@@ -1,50 +1,28 @@
 from app import db
+from app.models.StringPKBase import StringPKBase
 
-class Machine(db.Model):
+class Machine(StringPKBase):
     __tablename__ = 'machine'
-    id = db.Column(db.Integer, primary_key = True)
-    
-    name = db.Column(db.String)
-    
-    supported_mold_type = db.Column(db.Integer)
-    
-    installed_mold_id = db.Column(db.Integer)
-    
-    status = db.Column(db.String)
-    
-    downtime_start = db.Column(db.Date)
-    
-    downtime_end = db.Column(db.Date)
-    
-    total_downtime = db.Column(db.Integer)
-    
-    created_at = db.Column(db.Date)
-    
-    modified_at = db.Column(db.Date)
-    
-    supervisor_attention = db.Column(db.Integer)
 
-    # 1-1 relationship
+    name = db.Column(db.String, nullable=False, unique=True)
+
+    status = db.Column(db.String, default='Available')
+
+    ''' 1-1 relationship
     order = db.relationship(
         'Order',
         backref='machine',
         uselist=False
     )
+    '''
 
     def to_dict(self):
         return dict(
-            name = self.name,
-            #supported_mold_type = self.supported_mold_type,
-            #installed_mold_id = self.installed_mold_id,
-            status = self.status,
-            #downtime_start = self.downtime_start.isoformat(),
-            #downtime_end = self.downtime_end.isoformat(),
-            #total_downtime = self.total_downtime,
-            created_at = str(self.created_at),
-            modified_at = str(self.modified_at),
-            supervisor_attention = self.supervisor_attention,
-            id = self.id,
-            order = self.order
+            id=self.id,
+            name=self.name,
+            status=self.status,
+            created_at=self.created_at.isoformat(),
+            updated_at=self.updated_at.isoformat()
         )
 
     def __repr__(self):
