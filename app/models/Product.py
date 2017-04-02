729c31ec4831bd6ff3670b1664158e768ff34e78
Sithu Aung <sithu_aung@intuit.com>
2017-04-01 22:21:12 -0700

    m-m relationship between Product and Color

--- app/models/Product.py
+++ app/models/Product.py
@@ -3,19 +3,28 @@ from datetime import datetime
 from sqlalchemy import text
 from RawMaterial import RawMaterial
 from Base import Base
+from StringPKBase import StringPKBase
 
-class Product(Base):
+colors = db.Table(
+    'product_color',
+    db.Column('product_id', db.String, db.ForeignKey('product.id')),
+    db.Column('color_id', db.String, db.ForeignKey('color.id'))
+)
+
+class Product(StringPKBase):
     __tablename__ = 'product'
     name = db.Column(db.String)
     weight = db.Column(db.Integer)
     time_to_build = db.Column(db.Integer)
     selling_price = db.Column(db.Integer)
-    color = db.Column(db.String)
     num_employee_required = db.Column(db.Integer)
     mold_id = db.Column(db.Integer)
-    # 1-m
-    raw_material_id = db.Column(db.ForeignKey('raw_material.id'))
-    #rawmaterial = db.relationship(RawMaterial, backref='rawmaterials')
+    # m-m
+    colors = db.relationship(
+        'Color',
+        secondary=colors,
+        backref=db.backref('products', lazy='dynamic')
+    )
 
     def to_dict(self):
         """
@@ -28,12 +37,11 @@ class Product(Base):
             weight=self.weight,
             time_to_build=self.time_to_build,
             selling_price=self.selling_price,
-            color=self.color,
+            colors=self.colors,
             num_employee_required=self.num_employee_required,
             created_at=str(self.created_at),
             updated_at=str(self.updated_at),
-            mold_id=self.mold_id,
-            raw_material_id=self.raw_material_id
+            mold_id=self.mold_id
         )
 
     def __repr__(self):
---
+++ migrations/versions/70f7e713729f_.py
@@ -0,0 +1,49 @@
+"""empty message
+
+Revision ID: 70f7e713729f
+Revises: 589d846f900c
+Create Date: 2017-03-19 23:32:05.248356
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '70f7e713729f'
+down_revision = '589d846f900c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    print "creating product table..."
+    op.create_table(
+        'product',
+        sa.Column('id', sa.String(), nullable=False),
+        sa.Column('created_at', sa.DateTime(), nullable=True, server_default=sa.func.current_timestamp()),
+        sa.Column('updated_at', sa.DateTime(), nullable=True, server_default=sa.func.current_timestamp()),
+        sa.Column('name', sa.String(), nullable=False),
+        sa.Column('weight', sa.Integer(), nullable=False),
+        sa.Column('time_to_build', sa.Integer(), nullable=False),
+        sa.Column('selling_price', sa.Integer(), nullable=True),
+        sa.Column('num_employee_required', sa.Integer(), nullable=True),
+        sa.Column('mold_id', sa.Integer(), nullable=True),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('name')
+    )
+
+    print "creating product_color table..."
+    op.create_table(
+        'product_color',
+        sa.Column('product_id', sa.String(), nullable=False),
+        sa.Column('color_id', sa.String(), nullable=False),
+        sa.ForeignKeyConstraint(['color_id'], ['color.id']),
+        sa.ForeignKeyConstraint(['product_id'], ['product.id'])
+    )
+    ### end Alembic commands ###
+
+
+def downgrade():
+    op.drop_table('product_color')
+    op.drop_table('product')
