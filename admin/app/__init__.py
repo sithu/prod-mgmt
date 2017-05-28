6ad2306eaddec411923c91dd507e37152c74e801
Sithu Aung <sithu_aung@intuit.com>
2017-05-28 00:26:21 -0700

    custom template with Flask Admin

--- admin/app/__init__.py
+++ admin/app/__init__.py
@@ -6,29 +6,13 @@ from flask import Flask, request, session
 from flask import Flask, render_template
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from flask_babelex import Babel
 
 LOG = getLogger(__name__)
 
 app = Flask(__name__, static_url_path='/static')
 db = SQLAlchemy(app)
-babel = Babel(app)
 admin = Admin(
     app, 
     name='Popular Plastic', 
-    template_mode='bootstrap3',
-    category_icon_classes={
-        'color': 'glyphicon glyphicon-user',
-    }
+    template_mode='bootstrap3'
 )
-
-
-@babel.localeselector
-def get_locale():
-    """get locale from query param"""
-    override = request.args.get('lang')
-
-    if override:
-        session['lang'] = override
-
-    return session.get('lang', 'en')
